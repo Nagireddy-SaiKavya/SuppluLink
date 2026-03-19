683adf3b2df640cd1460fd2b8846ef372250bc54
Nagireddy-SaiKavya <dummy@gmail.com>
2026-03-19 04:34:30 +0000

    Day23

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';  
+import { AppRoutingModule } from './app-routing.module';
+
+// Feature module
+import { SupplyLinkModule } from './supplylink/supplylink.module';
+
+// Auth interceptor
+import { AuthInterceptor } from './auth.interceptors';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule 
+    AppRoutingModule,
+    SupplyLinkModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
